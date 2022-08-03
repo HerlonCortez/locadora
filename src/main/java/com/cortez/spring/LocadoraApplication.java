28e8b0895f60af3ed6f0ead89a23f2d6730463e4
Herlon <herlon@corepay.me>
2022-08-03 16:36:09 -0300

    quarto commit

--- src/main/java/com/cortez/spring/LocadoraApplication.java
+++ src/main/java/com/cortez/spring/LocadoraApplication.java
@@ -1,9 +1,13 @@
 package com.cortez.spring;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class LocadoraApplication {
 
 	public static void main(String[] args) {
